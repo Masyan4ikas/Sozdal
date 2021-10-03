4d69c3442af045890445aa57c18f2cd4005985d6
Masyan4ikas <90957473+Masyan4ikas@users.noreply.github.com>
2021-10-04 00:14:53 +0300

    Update zad1.cpp
    Даны стороны прямоугольника  a и b. Найти его площадь S=a*b и периметр P=2*(a+b).

--- Laba4/zad1.cpp
+++ Laba4/zad1.cpp
@@ -1,2 +1,18 @@
 Даны стороны прямоугольника  a и b. Найти его площадь S=a*b и периметр P=2*(a+b).
   
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a,b;
+    cout << "a=";
+    cin >> a;
+    cout << "b=";
+    cin >> b;
+ 
+    float s = a*b;
+    cout << "s=" << s << endl;
+    float p = 2*(a+b);
+    cout << "p=" << p << endl;
+    return 0;
+}
